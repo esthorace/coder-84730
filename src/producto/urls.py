4a910b5dc7e802fa8ae455caf8524af548974c68
Esteban <esteban.horacio.acevedo@gmail.com>
2026-02-23 22:34:16 -0300

    feat: CRUD basado en vistas de clase

--- src/producto/urls.py
+++ src/producto/urls.py
@@ -5,8 +5,14 @@ from producto import views
 app_name = "producto"
 
 urlpatterns = [
-    path("lista/", views.categoria_list, name="lista"),
-    path("crear/", views.categoria_create, name="crear"),
-    path("detalle/<int:pk>", views.categoria_detail, name="detalle"),
-    path("borrar/<int:pk>", views.categoria_delete, name="borrar"),
+    # path("lista/", views.categoria_list, name="lista"),
+    # path("crear/", views.categoria_create, name="crear"),
+    # path("cambiar/<int:pk>", views.categoria_update, name="cambiar"),
+    # path("detalle/<int:pk>", views.categoria_detail, name="detalle"),
+    # path("borrar/<int:pk>", views.categoria_delete, name="borrar"),
+    path("cambiar/<int:pk>", views.CategoriaUpdate.as_view(), name="cambiar"),
+    path("lista/", views.CategoriaList.as_view(), name="lista"),
+    path("crear/", views.CategoriaCreate.as_view(), name="crear"),
+    path("detalle/<int:pk>", views.CategoriaDetail.as_view(), name="detalle"),
+    path("borrar/<int:pk>", views.CategoriaDelete.as_view(), name="borrar"),
 ]
--- src/producto/views.py
+++ src/producto/views.py
@@ -1,39 +1,84 @@
 from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 
 from . import models
 from . import forms
 
 
-def categoria_list(request):
-    consulta = request.GET.get("consulta")
-    if consulta:
-        categorias = models.Categoria.objects.filter(nombre__contains=consulta)
-    else:
-        categorias = models.Categoria.objects.all()
-    return render(request, "producto/categoria_list.html", {"categorias": categorias})
+# def categoria_list(request):
+#     consulta = request.GET.get("consulta")
+#     if consulta:
+#         categorias = models.Categoria.objects.filter(nombre__contains=consulta)
+#     else:
+#         categorias = models.Categoria.objects.all()
+#     return render(request, "producto/categoria_list.html", {"categorias": categorias})
+
+class CategoriaList(ListView):
+    model = models.Categoria
+    # template_name = "producto/categoria_list.html"
+    # context_object_name = "categorias"
+
+    def get_queryset(self):
+        consulta = self.request.GET.get("consulta")
+        if consulta:
+            categorias = models.Categoria.objects.filter(nombre__contains=consulta)
+        else:
+            categorias = models.Categoria.objects.all()
+        return categorias
 
 
-def categoria_detail(request, pk: int):
-    categoria = models.Categoria.objects.get(id=pk)
-    return render(request, "producto/categoria_detail.html", {"categoria": categoria})
+
+# def categoria_detail(request, pk: int):
+#     categoria = models.Categoria.objects.get(id=pk)
+#     return render(request, "producto/categoria_detail.html", {"categoria": categoria})
+
+class CategoriaDetail(DetailView):
+    model = models.Categoria
 
 
-def categoria_delete(request, pk: int):
-    categoria = models.Categoria.objects.get(id=pk)
-    if request.method == "POST":
-        categoria.delete()
-        return redirect("producto:lista")
-    contexto = {"categoria": categoria}
-    return render(request, "producto/categoria_confirm_delete.html", contexto)
+# def categoria_delete(request, pk: int):
+#     categoria = models.Categoria.objects.get(id=pk)
+#     if request.method == "POST":
+#         categoria.delete()
+#         return redirect("producto:lista")
+#     contexto = {"categoria": categoria}
+#     return render(request, "producto/categoria_confirm_delete.html", contexto)
 
+class CategoriaDelete(DeleteView):
+    model = models.Categoria
+    success_url = reverse_lazy("producto:lista")
 
-def categoria_create(request):
-    if request.method == "POST":
-        form = forms.CategoriaForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("producto:lista")
-    else:
-        form = forms.CategoriaForm()
+# def categoria_create(request):
+#     if request.method == "POST":
+#         form = forms.CategoriaForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("producto:lista")
+#     else:
+#         form = forms.CategoriaForm()
    
-    return render(request, "producto/categoria_create.html", {"form": form})
+#     return render(request, "producto/categoria_create.html", {"form": form})
+
+class CategoriaCreate(CreateView):
+    model = models.Categoria
+    form_class = forms.CategoriaForm
+    success_url = reverse_lazy("producto:lista")
+
+
+# def categoria_update(request, pk: int):
+#     categoria = models.Categoria.objects.get(id=pk)
+#     if request.method == "GET":
+#         form = forms.CategoriaForm(instance=categoria)
+#     if request.method == "POST":
+#         form = forms.CategoriaForm(request.POST, instance=categoria)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("producto:lista")
+#     return render(request, "producto/categoria_form.html", {"form": form})
+
+
+class CategoriaUpdate(UpdateView):
+    model = models.Categoria
+    form_class = forms.CategoriaForm
+    success_url = reverse_lazy("producto:lista")
