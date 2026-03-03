d2d7080675ad1135603b44156ae58620eca2962b
Esteban <esteban.horacio.acevedo@gmail.com>
2026-03-02 21:30:32 -0300

    feat: Nuevo modelo Producto

--- src/producto/admin.py
+++ src/producto/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
-from .models import Categoria
+from .models import Categoria, Producto
 
 admin.site.register(Categoria)
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ("categoria", "nombre", "stock", "unidad_de_medida", "precio")
+    list_filter = ("categoria", "unidad_de_medida")
+    search_fields = ("nombre", "categoria__nombre")
--- src/producto/models.py
+++ src/producto/models.py
@@ -7,3 +7,25 @@ class Categoria(models.Model):
 
     def __str__(self):
         return f"{self.nombre}"
+
+
+class Producto(models.Model):
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="categoría")
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(null=True, blank=True)
+    unidad_de_medida = models.CharField(max_length=50)
+    stock = models.FloatField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha_actualizacion = models.DateTimeField(editable=False, auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.nombre} ({self.unidad_de_medida}) ${self.precio}"
+    
+    class Meta:
+        verbose_name = "Producto"
+        verbose_name_plural = "Productos"
+        constraints = [
+            models.UniqueConstraint(fields=["categoria", "nombre"], name="categoria_nombre")
+        ]
+        indexes = [models.Index(fields=["nombre"])]
+        
